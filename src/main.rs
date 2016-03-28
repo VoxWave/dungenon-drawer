83579c7d0ed62c9811b3f8da0c4969c1828f39a9
Victor Bankowski <victor_bankowski@hotmail.com>
2016-03-29 00:12:16 +0300

    added commandline param fetch and image lib to deps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate image;
+
+use std::env;
+
+use image::png::PNGEncoder;
+
 fn main() {
-    println!("Hello, world!");
+    let params = get_command_line_params();
+}
+
+fn get_command_line_params() -> Vec<String> {
+    let mut arguments = Vec::new();
+    for argument in env::args() {
+        arguments.append(argument.to_string());
+    }
+    arguments
 }
