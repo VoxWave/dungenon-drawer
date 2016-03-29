93427f3ab50c232d922819817a51ccf20bdefe3d
Victor Bankowski <victor_bankowski@hotmail.com>
2016-03-29 22:52:20 +0300

    some decided to make the program interactive instead of using commandline parameters

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
 extern crate image;
+extern crate dungenon;
 
-use std::env;
+use std::io;
 
 use image::png::PNGEncoder;
 
 fn main() {
-    let params = get_command_line_params();
+    let mut level = init_level();
+
+    println!("Type help for list of commands.");
+    loop {
+        println!("Enter command:");
+        let mut line = io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+        match line {
+            "Dungeon" => {
+
+            },
+            "Maze" => {
+
+            },
+            "Room" => {
+
+            },
+            "Exit" => {break}
+        }
+    }
 }
 
-fn get_command_line_params() -> Vec<String> {
-    let mut arguments = Vec::new();
-    for argument in env::args() {
-        arguments.append(argument.to_string());
-    }
-    arguments
+fn init_level() -> Level {
+
 }
