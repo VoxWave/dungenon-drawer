326ec9d7c14ea9fa0181c58b692b4397d1040ce1
Victor Bankowski <victor_bankowski@hotmail.com>
2016-04-11 00:05:13 +0300

    color change function done

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate image;
 extern crate dungenon;
+extern crate cast;
 
 use std::io;
 use std::path::PathBuf;
@@ -11,128 +12,173 @@ use dungenon::generator::MazeGen;
 use image::RgbImage;
 use image::Rgb;
 
-struct Drawer {
-    
-}
+use std::collections::HashMap;
 
 fn main() {
-    println!("Initializing level...");
-    let mut level = init_level();
+    Drawer::new().start();
+}
 
-    println!("This is the dungenon-drawer. \nType Help for list of commands.");
-    loop {
-        println!("Enter command:");
-        let mut command = string_from_cmd();
-        println!("{}", command);
-        match command.trim() {
-            "dungeon" => carve_dungeon(&mut level),
-            "maze" => carve_maze(&mut level),
-            "room" => carve_rooms(&mut level),
-            "reset" => level = init_level(),
-            "export" => {
-                png_export(&mut level);
-            },
-            "print" => print_level(&level),
-            "help" => {
-                println!("Help command is not implemented yet!");
-            }
-            "exit" => break,
-            _ => println!("Invalid command"),
+struct Drawer {
+    pub colors: HashMap<Tile, Rgb<u8>>,
+}
+
+impl Drawer {
+    pub fn new() -> Drawer {
+        let mut colors = HashMap::new();
+        colors.insert(Tile::Wall, Rgb([127u8, 127u8, 127u8]));
+        colors.insert(Tile::Floor, Rgb([200u8, 200u8, 200u8]));
+        colors.insert(Tile::Void, Rgb([0u8, 0u8, 0u8]));
+        Drawer{
+            colors: colors,
         }
     }
-}
 
-fn init_level() -> Level {
-    println!("Input level width: ");
-    let mut x = usize_from_cmd();
+    pub fn start(&mut self) {
+        println!("Initializing level...");
+        let mut level = self.init_level();
 
-
-    println!("Input level height: ");
-    let mut y = usize_from_cmd();
-
-    Level::new_filled_with(Some(Tile::Wall), x, y)
-}
-
-fn carve_dungeon(level: &mut Level) {
-    println!("Not implemented.");
-}
-
-fn carve_maze(level: &mut Level) {
-    println!("Input MazeGen startpos x coordinate: ");
-    let mut x = usize_from_cmd();
-
-    println!("Input MazeGen startpos y coordinate: ");
-    let mut y = usize_from_cmd();
-    let mut mazegen = MazeGen::new(x,y);
-    level.apply(|m| mazegen.generate(m));
-}
-
-fn carve_rooms(level: &mut Level) {
-    println!("Not implemented.");
-}
-
-fn png_export(level: &mut Level) {
-    let mut level_image = RgbImage::new(level.get_height() as u32, level.get_height() as u32);
-    for y in 0 .. level.get_width() {
-        for x in 0 .. level.get_height() {
-            level_image.put_pixel(x as u32, y as u32, tile_to_color(&level[(x,y)]));
-        }
-    }
-    let mut p = PathBuf::new();
-    println!("Enter png name:");
-    p.push(string_from_cmd().trim());
-    p.set_extension("png");
-    level_image.save(p.as_path());
-}
-
-fn tile_to_color(tile: &Option<Tile>) -> Rgb<u8> {
-    match *tile {
-        Some(ref tile) => {
-            match *tile {
-                Tile::Wall => Rgb([127u8, 127u8, 127u8]),
-                Tile::Floor => Rgb([200u8, 200u8, 200u8]),
-                Tile::Void => Rgb([0u8, 0u8, 0u8]),
-            }
-        },
-        None => Rgb([0u8, 0u8, 0u8]),
-    }
-}
-
-
-fn print_level(level: &Level) {
-    let mut string = String::new();
-    for y in 0 .. level.get_width() {
-        for x in 0 .. level.get_height() {
-            match level[(x,y)] {
-                Some(ref tile) => {
-                    match tile {
-                        &Tile::Wall => string.push('#'),
-                        &Tile::Floor => string.push(' '),
-                        &Tile::Void => string.push('*'),
-                    }
+        println!("This is the dungenon-drawer. \nType Help for list of commands.");
+        loop {
+            println!("Enter command:");
+            let command = Self::string_from_cmd();
+            match command.trim() {
+                "dungeon" => Self::carve_dungeon(&mut level),
+                "maze" => self.carve_maze(&mut level),
+                "room" => Self::carve_rooms(&mut level),
+                "reset" => level = self.init_level(),
+                "colors" => self.change_colors(),
+                "export" => {
+                    self.png_export(&mut level);
                 },
-                None => string.push('*'),
+                "print" => Self::print_level(&level),
+                "help" => {
+                    println!("Help command is not implemented yet!");
+                }
+                "exit" => break,
+                _ => println!("Invalid command."),
             }
         }
-        string.push('\n');
     }
-    println!("{}", &string);
-}
 
-fn usize_from_cmd() -> usize {
-    let mut num = String::new();
+    fn change_colors(&mut self) {
+        use cast;
+        use std::u8;
+        loop {
+            println!("Which color would you like to change? (floor, wall, void), Type exit if you're done with modifying colors.");
+            let command = Self::string_from_cmd();
+            let key = match command.trim() {
+                "floor" => Tile::Floor,
+                "wall" => Tile::Wall,
+                "void" => Tile::Void,
+                "exit" => break,
+                _ => {
+                    println!("Invalid command.");
+                    continue;
+                },
+            };
+            println!("Input red value for the color:");
+            let r = cast::u8(Self::usize_from_cmd()).unwrap_or(u8::MAX);
 
-    io::stdin().read_line(&mut num)
-        .expect("failed to read line");
+            println!("Input green value for the color:");
+            let g = cast::u8(Self::usize_from_cmd()).unwrap_or(u8::MAX);
 
-    let num: usize = num.trim().parse()
-        .expect("Please type a number!");
-    num
-}
+            println!("Input blue value for the color:");
+            let b = cast::u8(Self::usize_from_cmd()).unwrap_or(u8::MAX);
+            self.colors.insert(key, Rgb([r,g,b]));
+        }
+    }
 
-fn string_from_cmd() -> String {
-    let mut string = String::new();
-    io::stdin().read_line(&mut string)
-    .expect("Failed to read line");
-    string
+    fn init_level(&self) -> Level {
+        println!("Input level width: ");
+        let x = Self::usize_from_cmd();
+
+
+        println!("Input level height: ");
+        let y = Self::usize_from_cmd();
+
+        Level::new_filled_with(Some(Tile::Wall), x, y)
+    }
+
+    fn carve_dungeon(level: &mut Level) {
+        println!("Not implemented.");
+    }
+
+    fn carve_maze(&self, level: &mut Level) {
+        println!("Input MazeGen startpos x coordinate: ");
+        let x = Self::usize_from_cmd();
+
+        println!("Input MazeGen startpos y coordinate: ");
+        let y = Self::usize_from_cmd();
+        let mut mazegen = MazeGen::new(x,y);
+        level.apply(|m| mazegen.generate(m));
+    }
+
+    fn carve_rooms(level: &mut Level) {
+        println!("Not implemented.");
+    }
+
+    fn png_export(&self, level: &mut Level) {
+        let mut level_image = RgbImage::new(level.get_width() as u32, level.get_height() as u32);
+        for y in 0 .. level.get_width() {
+            for x in 0 .. level.get_height() {
+                level_image.put_pixel(x as u32, y as u32, self.tile_to_color(&level[(x,y)]));
+            }
+        }
+        let mut p = PathBuf::new();
+        println!("Enter png name:");
+        p.push(Self::string_from_cmd().trim());
+        p.set_extension("png");
+        level_image.save(p.as_path()).expect("Something went wrong when saving the png.");
+    }
+
+    fn tile_to_color(&self, tile: &Option<Tile>) -> Rgb<u8> {
+        match *tile {
+            Some(ref tile) => {
+                    match self.colors.get(tile) {
+                        Some(color) => color.clone(),
+                        None => Rgb([255u8, 0u8, 255u8]),
+                    }
+            },
+            None => Rgb([0u8, 0u8, 0u8]),
+        }
+    }
+
+
+    fn print_level(level: &Level) {
+        let mut string = String::new();
+        for y in 0 .. level.get_width() {
+            for x in 0 .. level.get_height() {
+                match level[(x,y)] {
+                    Some(ref tile) => {
+                        match tile {
+                            &Tile::Wall => string.push('#'),
+                            &Tile::Floor => string.push(' '),
+                            &Tile::Void => string.push('*'),
+                        }
+                    },
+                    None => string.push('*'),
+                }
+            }
+            string.push('\n');
+        }
+        println!("{}", &string);
+    }
+
+    fn usize_from_cmd() -> usize {
+        let mut num = String::new();
+
+        io::stdin().read_line(&mut num)
+            .expect("failed to read line");
+
+        let num: usize = num.trim().parse()
+            .expect("Please type a number!");
+        num
+    }
+
+    fn string_from_cmd() -> String {
+        let mut string = String::new();
+        io::stdin().read_line(&mut string)
+        .expect("Failed to read line");
+        string
+    }
 }
